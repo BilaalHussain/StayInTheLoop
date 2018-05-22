015736900b6b424b80d3cf4d57b6b58443887e7b
Bilaal Hussain <Bilaalhussain1@gmail.com>
2018-05-22 06:15:02 -0400

    Used NLTK library to implement title filtering
    Remove common nouns, adverbs, adjectives, prepositions, conjunctions and interjections

--- First Bot v0.1.py
+++ First Bot v0.1.py
@@ -6,12 +6,57 @@ Date         - 5/15/2018
 """
 
 import praw
-
-reddit = praw.Reddit('bot1')
+import nltk
+from nltk.corpus import stopwords
+	
+reddit = praw.Reddit('bot1', user_agent='First Bot v0.1')
 subreddit = reddit.subreddit("outoftheloop")
 
-for submission in subreddit.hot(limit=5):
-    print("Title: ", submission.title)
-    print("Text: ", submission.selftext)
-    print("Score: ", submission.score)
-    print("----------------------------\n")
+#Data storage
+post_ids = []
+post_titles = []
+post_responses = []
+
+
+#Query limit
+limit = 5
+
+
+
+current_iterations = 0
+
+#Purpose: filter_dictionary_words(text): Returns text with all common words from the Unix usr/share/dict/words dictionary
+#	removed. Uses this to find any words that don't appear in the english language.
+#Contract: ListOf(String) -> ListOf(String)
+#This will often find the most important words in a reddit post. Often referring to the subject.
+def filter_dictionary_words(text):
+    text_vocab = set(w.lower() for w in text if w.isalpha())
+    english_vocab = set(w.lower() for w in nltk.corpus.words.words())
+    unusual = text_vocab - english_vocab
+    return sorted(unusual)
+
+#Purpose: filter_dictionary_words(text): Returns text with high frequency words with minimal lexical context filtered out
+#Contract: ListOf(String) -> ListOf(String)
+#This is a less aggressive version of the filter_dictionary_words function
+def filter_common_words(text):
+    text_vocab = set(w.lower() for w in text if w.isalpha())
+    common_vocab = set(w.lower() for w in nltk.corpus.stopwords.words('english'))
+    uncommon = text_vocab - common_vocab
+    return sorted(uncommon)
+	
+	
+
+for post in subreddit.hot(limit=1):
+	post_titles.append(post.title)
+	for tokenized in post_titles:
+		print(filter_common_words(tokenized))
+	
+
+
+
+	
+#
+  #  print("Title: ", submission.title)
+  #  print("Text: ", submission.selftext)
+  #  print("Score: ", submission.score)
+  #  print("----------------------------\n")
